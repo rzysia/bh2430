e76e7de4d8fa04ae3905d775373e42472ff9a2be
rzysia <rzysia@Krzysztof>
2012-11-06 15:10:25 +0100

    pierwsze zarysy obiektów

---
+++ bh2430/src/bh2430/Army.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bh2430;
+
+/**
+ *
+ * @author rzysia
+ */
+public class Army {
+    
+    //plan jest taki, ze pod indeksem 0 jest np f-18, jest typu np 0 (taki id typu, moze sie przydac), 
+    String[] nazwaTypu;
+    int[] typ;
+    int[] ilosc;
+}
---
+++ bh2430/src/bh2430/Sector.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bh2430;
+
+/**
+ *
+ * @author rzysia
+ */
+
+//na razie nazwy daje po polsku, jak komus bezidze z tym bardzo zle
+// i mu sie bedzie chcialo, niech przetlumaczy - bardzo prosto sie zmiania nazwe, zaznaczasz nazwe przy deklaracji,
+//wchodzisz w refactor i tam dajesz rename ;)
+public class Sector {
+    int id;     //indeks, pod ktorym bedzie w tablicy sektorow
+    String name;    //nazwa sektora - generowana jakos tam, ktos cos mowil o jakims filmie s-f :P
+    int rozmiar;    //oczywiste chyba
+    int idWlasciciela;  //kto jest wlascicielem danego sektora
+    Army wojsko;   //ile i jakich typow jednostki tutaj staconuja
+    Sector[] sasiedzi;  //lista sasiadow
+    boolean czyKocostam;    //czy jest w koczymstam
+    
+    void zaatakuj(Army atakujacy)
+    {
+        //tu pewnie wywolanie funkcji z dwoma argumentami, mianowicie z armia atakujacego i armia broniacego
+        // - wszystko jeszcze wyjdzie w praniu
+    }
+}
