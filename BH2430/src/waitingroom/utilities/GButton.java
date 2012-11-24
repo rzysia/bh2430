e7ef6aa2c72222e9f1a5b156311a347b84fe4613
PrzemyslawMalinowski <przemyslaw.malinowski@uj.edu.pl>
2012-11-24 17:41:56 +0100

    Napisałem pierwszą część silnika

--- BH2430/src/waitingroom/utilities/GButton.java
+++ BH2430/src/waitingroom/utilities/GButton.java
@@ -1,31 +1,52 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *@author
+ *  Przemysław Malinowski
+ * @version
+ *  0.0.1
+ * 
+ * Klasa przycisku graficznego, bazowany na JButton
  */
+
+
 package waitingroom.utilities;
+
 
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.MouseInfo;
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Rectangle2D;
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import waitingroom.WaitingRoomPanel;
 
 /**
  *
  * @author boroowa
  */
-public class GButton extends JButton{
+public class GButton extends JComponent implements MouseListener{
     
     private Graphics2D g2d;
+    
+    private int x, y , w , h;
+    
+    public GButton(int x, int y, int w, int h){
+        super();
+        
+        this.x = x; this.y = y;
+        this.w = w; this.h = h;
+        
+        this.setLocation(new Point(x,y));
+        
+        enableInputMethods(true);   
+        addMouseListener(this);
+        
+        System.out.println("olsidfjoisjdfopijdfovinjodifnvoindfvoifndv");
+        
+    }
     
     @Override
     public void paintComponent(Graphics g){
@@ -33,7 +54,37 @@ public class GButton extends JButton{
         
         g2d = (Graphics2D) g;
         
-        Rectangle2D a = new Rectangle2D.Double(TOP, TOP, TOP, TOP);
+        Rectangle2D a = new Rectangle2D.Double( x,
+                                                y,
+                                                w,
+                                                h);
+        
+        g2d.setColor(Color.red);
+        g2d.fill(a);
     }
-   
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("asdasdasdasdasd");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("asdasdasdasdasd");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
