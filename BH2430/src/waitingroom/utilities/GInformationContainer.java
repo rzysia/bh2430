1c2194fd57ccc37a59a40e2c7d6d530558444fec
PrzemyslawMalinowski <przemyslaw.malinowski@uj.edu.pl>
2012-11-25 19:26:55 +0100

    Dodałem kontener z danymi

---
+++ BH2430/src/waitingroom/utilities/GInformationContainer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package waitingroom.utilities;
+
+/**
+ *
+ * @author boroowa
+ */
+public class GInformationContainer {
+    
+    public GButtonGraphics buttonsGraphics;
+    public GFonts fonts;
+    
+}
