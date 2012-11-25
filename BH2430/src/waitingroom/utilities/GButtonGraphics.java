8b3c074213c9872684e818f6f5787dc303d27025
PrzemyslawMalinowski <przemyslaw.malinowski@uj.edu.pl>
2012-11-25 17:51:34 +0100

    Nowy mechanizm pobierania przez przyciski obrazków

---
+++ BH2430/src/waitingroom/utilities/GButtonGraphics.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package waitingroom.utilities;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author boroowa
+ */
+public class GButtonGraphics {
+    
+    BufferedImage normal;
+    BufferedImage over;
+    BufferedImage press;
+    
+    
+    
+}
