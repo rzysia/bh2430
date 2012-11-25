64b9c05ac8b19ff068d9a267e2a74b3e0bf91989
PrzemyslawMalinowski <przemyslaw.malinowski@uj.edu.pl>
2012-11-25 17:51:01 +0100

    Usunięcie bugów w przycisku

--- BH2430/src/waitingroom/utilities/GButton.java
+++ BH2430/src/waitingroom/utilities/GButton.java
@@ -40,8 +40,6 @@ public class GButton extends JComponent implements MouseListener, MouseMotionLis
      * POLA
      */
     
-    private Graphics2D g2d;
-    
     private int x, y , w , h;
     
     private Rectangle2D rect;
@@ -60,130 +58,66 @@ public class GButton extends JComponent implements MouseListener, MouseMotionLis
      * FUNKCJE
      */
     
-    public GButton(String text, int x, int y){
-        super();
-        
-        //ustawianie pol
-            this.x = x; this.y = y - 50;
-            this.w = 270; this.h = 100;
-        
-        //włączanie zdarzeń w oknie
-            enableInputMethods(true);   
-            addMouseListener(this);
-            addMouseMotionListener(this);
-            
-        
-        //wczytywanie zmiennych
-            try {
-                
-                buttonOver = ImageIO.read(new File("./graphics/MainMenu/over_button.png"));
-                buttonNormal = ImageIO.read(new File("./graphics/MainMenu/unpress_button.png"));
-                buttonPressed = ImageIO.read(new File("./graphics/MainMenu/press_button.png"));
-                
-            } catch (IOException e) {
-                
-                System.out.println("Problem with loading button textures");
-                System.exit(-1);
-                return;
-                
-            }
-            
-            button = buttonNormal;
-            
-        //wczytanie czcionków
-            try {
-            
-             font= Font.createFont(Font.TRUETYPE_FONT, new File("./fonts/D3Euronism.ttf"));
-
-        } catch (IOException|FontFormatException e) {
-             System.out.println("Cannot read True Type Font files to button");
-             System.exit(-1);
-        }
-            
-        //dodanie texta
-            this.text = text;
-   
-    }
-    
     public GButton(int x, int y){
         super();
         
         //ustawianie pol
-            this.x = x; this.y = y - 50;
+            this.x = x; this.y = y;
             this.w = 270; this.h = 100;
+            
+            this.setBounds(x, y, w, h);
+            
+            //this.setOpaque(true);
+            this.setForeground(Color.GREEN);
+            this.setBackground(Color.red);
+            
+            this.setPreferredSize(new Dimension(w,h));
         
         //włączanie zdarzeń w oknie
             enableInputMethods(true);   
             addMouseListener(this);
             addMouseMotionListener(this);
-            
-        
-        //wczytywanie zmiennych
-            try {
-                
-                buttonOver = ImageIO.read(new File("./graphics/MainMenu/over_button.png"));
-                buttonNormal = ImageIO.read(new File("./graphics/MainMenu/unpress_button.png"));
-                buttonPressed = ImageIO.read(new File("./graphics/MainMenu/press_button.png"));
-                
-            } catch (IOException e) {
-                
-                System.out.println("Problem with loading button textures");
-                System.exit(-1);
-                return;
-                
-            }
-            
-            button = buttonNormal;
-   
+      
+            setVisible(true);
     }
     
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         
-        g2d = (Graphics2D) g; 
-        g2d.drawImage(button, this.x, this.y, null);
+        Graphics2D g2d;
         
-        g2d.setColor(Color.WHITE);
-        g2d.setFont(font.deriveFont(28.0f));
-        g2d.drawString(text, this.x + 70, this.y + 50);
+        g2d = (Graphics2D) g; 
+        g2d.setColor(color);
+        g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
         
     }
     
     @Override
     public void mouseClicked(MouseEvent e) {
-        //button = this.buttonPressed;
-        //repaint();
+        //TODO
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        
-        if( (e.getLocationOnScreen().x <= this.x + this.w) &&
-                (e.getLocationOnScreen().x >= this.x) &&
-                (e.getLocationOnScreen().y >= this.y + 50) &&
-                (e.getLocationOnScreen().y <= this.y + this.h + 50)){
-            
-                    button = this.buttonPressed;
-                    repaint();
-                    
-        }
+        //TODO
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-       button = this.buttonNormal;
-       repaint();
+       //TODO
     }
     
     @Override
     public void mouseEntered(MouseEvent e) {
-        //TODO   
+        color = Color.red;
+        repaint();
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet.");
+        color = Color.GREEN;
+        repaint();
     }
 
     @Override
@@ -193,25 +127,6 @@ public class GButton extends JComponent implements MouseListener, MouseMotionLis
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        
-        //jeżeli to badziewie jest w śrosku
-        
-            if( (e.getLocationOnScreen().x <= this.x + this.w) &&
-                (e.getLocationOnScreen().x >= this.x) &&
-                (e.getLocationOnScreen().y >= this.y + 50) &&
-                (e.getLocationOnScreen().y <= this.y + this.h + 50)){
-                
-                //System.out.println(e.getLocationOnScreen().x + " " + e.getLocationOnScreen().y);
-        
-                button = this.buttonOver;
-                repaint();
-                
-                
-            } else {
-                
-                button = this.buttonNormal;
-                repaint();
-                
-            }
+        //TODO
     }
 }
