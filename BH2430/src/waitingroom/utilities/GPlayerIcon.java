cfb8a26101623666435f8a900d6a09cbcd7c4e68
PrzemyslawMalinowski <przemyslaw.malinowski@uj.edu.pl>
2012-11-25 22:50:23 +0100

    Zaktualizowanie kontrolki ikony gracza

--- BH2430/src/waitingroom/utilities/GPlayerIcon.java
+++ BH2430/src/waitingroom/utilities/GPlayerIcon.java
@@ -5,6 +5,7 @@
 package waitingroom.utilities;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -20,50 +21,71 @@ import javax.swing.JComponent;
 public class GPlayerIcon extends JComponent{
     
     private int x, y , w , h;
+    
     private String text;
     
     private boolean isPlayer;
     
-    private BufferedImage fieldEmpty;
-    private BufferedImage fieldUsed;
-    private BufferedImage field;
-    
     private Graphics2D g2d;
     
-    public GPlayerIcon(int x, int y){
-        super();
+    private BufferedImage surface;
+    
+    private GInformationContainer ginfo;
+    
+    public GPlayerIcon(int x, int y, GInformationContainer ginfo){
         
-        this.x = x; this.y = y - 50;
-        this.w = 300; this.h = 80;
+        //ustawienie nadkonstruktora
+            super();
         
-        try {
-                
-                fieldEmpty = ImageIO.read(new File("./graphics/WaitingRoomGraphics/grzaczNieaktywowany.png"));
-                fieldUsed = ImageIO.read(new File("./graphics/WaitingRoomGraphics/graczAktywowany.png"));
-                
-            } catch (IOException e) {
-                
-                System.out.println("Problem with loading button textures");
-                System.exit(-1);
-                return;
-                
-            }
-        
-        field = fieldEmpty;
+        //ustawienie pozycji
+            this.x = x; this.y = y;
+            this.w = 300; this.h = 80;
+            
+            this.setBounds(x, y, w, h);
+            this.setPreferredSize(new Dimension(w,h));
+            
+            surface = ginfo.playerIconGraphics.getEmptyGraphics();
+            
+        //ustawienie gracza
+            this.isPlayer = false;
     }
+    
+    /**
+     * Zmienia tekst w okięku
+     * @param text 
+     */
     
     public void setText(String text){
         this.text = text;
     }
     
+    /**
+     * ustawia, czy jest gracz, czy też nie
+     * @param player 
+     */
+    
+    public void usingPlace(boolean player){
+        
+        //ustawienie 
+            this.isPlayer = player;
+        
+        //ustawienie 
+            if(player){
+               surface = ginfo.playerIconGraphics.getUsedGraphics();
+               repaint();
+            } else {
+               surface = ginfo.playerIconGraphics.getEmptyGraphics();
+               repaint();
+            }
+    }
+    
     @Override
     public void paintComponent(Graphics g){
-        super.paintComponent(g);
+        super.paintComponent(g); 
         
         g2d = (Graphics2D) g;
         
-        g2d.drawImage(field, x, y, this);
-        
+        g2d.drawImage(surface,0,0,this);
     }
     
 }
