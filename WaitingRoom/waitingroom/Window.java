641e411b06fd8b5a6f3774f2b516ee012c0e5174
PrzemyslawMalinowski <przemyslaw.malinowski@uj.edu.pl>
2012-11-25 23:32:51 +0100

    Dodanie plików

--- WaitingRoom/waitingroom/Window.java
+++ WaitingRoom/waitingroom/Window.java
@@ -4,18 +4,23 @@
  */
 package waitingroom;
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
 /**
  *
  * @author boroowa
  */
-public class WaitingRoomDialog extends javax.swing.JDialog {
+public class Window extends javax.swing.JFrame {
 
     /**
-     * Creates new form WaitingRoomDialog
+     * Creates new form Window
      */
-    public WaitingRoomDialog(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
-        initComponents();
+    public Window() {
+        add(new WaitingRoomPanel());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        
     }
 
     /**
@@ -27,9 +32,7 @@ public class WaitingRoomDialog extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(800, 600));
-        setMinimumSize(new java.awt.Dimension(800, 600));
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -62,27 +65,21 @@ public class WaitingRoomDialog extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(WaitingRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(WaitingRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(WaitingRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(WaitingRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
-        /* Create and display the dialog */
+        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
-                WaitingRoomDialog dialog = new WaitingRoomDialog(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
+                new Window().setVisible(true);
             }
         });
     }
